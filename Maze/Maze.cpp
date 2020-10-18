045fe029fe6fe8970f56c870ff210a158a9fa1ba
iam-Abol <mh.z77w@gmail.com>
2020-10-18 20:14:22 +0330

    default constructor for stack class finished

--- Maze/Maze.cpp
+++ Maze/Maze.cpp
@@ -4,7 +4,9 @@ class Position{
 public:
 	int i , j;
 	Position *next;
-
+	Position(){
+	
+	}
 	Position(int i, int j) :i(i), j(j){};
 	~Position(){
 		delete next;
@@ -15,7 +17,10 @@ class stack{
 	int size;
 	
 public:
-	
+	stack(){
+		head = new Position;
+		size = 0;
+	}
 };
 int _tmain(int argc, _TCHAR* argv[])
 {
