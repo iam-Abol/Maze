22251a94eed82c0030056c3cc4960ca82faa2116
iam-Abol <mh.z77w@gmail.com>
2020-10-18 20:05:03 +0330

    the destructor for position class finished

--- Maze/Maze.cpp
+++ Maze/Maze.cpp
@@ -6,11 +6,20 @@ public:
 	Position *next;
 
 	Position(int i, int j) :i(i), j(j){};
-
+	~Position(){
+		delete next;
+	}
 };
-
+class stack{
+	Position *head;
+	int size;
+	
+public:
+	
+};
 int _tmain(int argc, _TCHAR* argv[])
 {
+	
 	return 0;
 }
 
